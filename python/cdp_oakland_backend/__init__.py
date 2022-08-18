ce91724431a730f7a05db3389b5191cc3840ffc3
CouncilDataProjectServiceAccount <councildataproject@gmail.com>
2022-08-18 00:10:53 +0000

    Initial commit

---
+++ python/cdp_oakland_backend/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Top-level package for Oakland
+CDP Instance backend.
+"""
+
+__author__ = "evamaxfield"
+__version__ = "1.0.0"
+
+
+def get_module_version() -> str:
+    return __version__
