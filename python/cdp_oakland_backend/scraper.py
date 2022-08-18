d9a85ef9cee35a219fa553f678df4f441ea67b80
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2022-08-17 18:16:19 -0700

    Ignore minutes items

--- python/cdp_oakland_backend/scraper.py
+++ python/cdp_oakland_backend/scraper.py
@@ -18,6 +18,27 @@ def get_events(
     scraper = LegistarScraper(
         client="oakland",
         timezone="America/Los_Angeles",
+        ignore_minutes_item_patterns=[
+            (
+                "the City Council has found that an imminent risk to the "
+                "health of attendees due to the COVID-19 pandemic continues to exist"
+            ),
+            (
+                "PUBLIC PARTICIPATION The public may observe and/or "
+                "participate in this meeting many ways"
+            ),
+            (
+                "Americans With Disabilities Act If you need special assistance "
+                "to participate in Oakland City Council"
+            ),
+            (
+                "THE HANGING OF BANNERS, POSTERS, SIGNS, OR ANY MATERIAL"
+            ),
+            (
+                "MATERIALS RELATED TO ITEMS ON THIS AGENDA SUBMITTED TO "
+                "THE CITY COUNCIL AFTER DISTRIBUTION"
+            ),
+        ],
     )
 
     return scraper.get_events(begin=from_dt, end=to_dt)
