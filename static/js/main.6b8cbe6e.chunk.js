b92208bd725e801f348bf1ea9e698cafa52960a0
evamaxfield <evamaxfield@users.noreply.github.com>
2023-01-30 23:30:46 +0000

    Deploying to gh-pages from @ CouncilDataProject/oakland@68efa3e1626458cf85dfe77c503253aead267f1a 🚀

--- static/js/main.6b8cbe6e.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpcdp-oakland"]=this["webpackJsonpcdp-oakland"]||[]).push([[0],{206:function(e,n){},91:function(e,n,i){"use strict";i.r(n);i(0);var o=i(26),t=i.n(o),c=i(66),a=(i(220),i(58));t.a.render(Object(a.jsx)("div",{children:Object(a.jsx)(c.AppConfigProvider,{appConfig:{firebaseConfig:{options:{projectId:"cdp-oakland-ba81c097"},settings:{}},municipality:{name:"Oakland",timeZone:"America/Los_Angeles",footerLinksSections:[]}},children:Object(a.jsx)(c.App,{})})}),document.getElementById("root"))}},[[91,1,2]]]);
-//# sourceMappingURL=main.6b8cbe6e.chunk.js.map
